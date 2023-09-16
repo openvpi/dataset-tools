28671027aab7645688a61ea8b409f04467687be2
笛鹿FlutyDeer <68263367+flutydeer@users.noreply.github.com>
2023-09-16 23:13:05 +0800

    Add round corner

--- src/tests/ParamEditTest/NoteGraphicsItem.cpp
+++ src/tests/ParamEditTest/NoteGraphicsItem.cpp
@@ -19,25 +19,25 @@ void NoteGraphicsItem::paint(QPainter *painter, const QStyleOptionGraphicsItem *
     pen.setColor(colorPrimary);
 
     auto rect = sceneBoundingRect();
-
-    pen.setWidthF(0);
-    painter->setPen(pen);
-    painter->setBrush(colorPrimary);
-    painter->drawRect(rect);
-
-    painter->scale(1.0 / scaleX, 1.0 / scaleY);
-
-    pen.setColor(QColor(255, 255, 255));
-    painter->setPen(pen);
     auto left = rect.left();
     auto top = rect.top();
     auto width = rect.width() * scaleX;
     auto height = rect.height() * scaleY;
-    auto textRect = QRectF(left, top, width, height);
+    auto scaledRect = QRectF(left, top, width, height);
+
+    pen.setWidthF(0);
+    painter->setPen(pen);
+    painter->setBrush(colorPrimary);
+
+    painter->scale(1.0 / scaleX, 1.0 / scaleY);
+    painter->drawRoundedRect(scaledRect, 4, 4);
+
+    pen.setColor(QColor(255, 255, 255));
+    painter->setPen(pen);
     auto font = QFont("Microsoft Yahei UI");
     font.setPointSizeF(10);
     painter->setFont(font);
-    painter->drawText(textRect, m_text, QTextOption(Qt::AlignVCenter));
+    painter->drawText(scaledRect, m_text, QTextOption(Qt::AlignVCenter));
 }
 
 void NoteGraphicsItem::setText(const QString &text) {
