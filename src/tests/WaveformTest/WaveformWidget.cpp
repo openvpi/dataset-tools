d80768b48a6a5116e424841ec7ca958ec554278c
笛鹿FlutyDeer <68263367+flutydeer@users.noreply.github.com>
2023-11-04 23:35:10 +0800

    Add waveform zoom and move

--- src/tests/WaveformTest/WaveformWidget.cpp
+++ src/tests/WaveformTest/WaveformWidget.cpp
@@ -29,19 +29,45 @@ void WaveformWidget::paintEvent(QPaintEvent *event) {
 
     QPen pen;
     pen.setColor(colorPrimary);
+    pen.setWidthF(2);
+    painter.setPen(pen);
+
+    painter.setBrush(colorPrimary);
+    painter.drawRoundedRect(rect(), 6, 6);
+    painter.setBrush(Qt::NoBrush);
+
+    pen.setColor(Qt::white);
     pen.setWidthF(1.5);
     painter.setPen(pen);
 
+    auto font = QFont("Microsoft Yahei UI");
+    font.setPointSizeF(10);
+    painter.setFont(font);
+    int padding = 2;
+    auto textRectLeft = rect().left() + padding;
+    auto textRectTop = rect().top() + padding;
+    auto textRectWidth = rect().width() - 2 * padding;
+    auto textRectHeight = rect().height() - 2 * padding;
+    auto textRect = QRectF(textRectLeft, textRectTop, textRectWidth, textRectHeight);
+    auto fontMetrics = painter.fontMetrics();
+    auto textHeight = fontMetrics.height();
+    painter.drawText(textRect, m_clipName, QTextOption(Qt::AlignTop));
+
     if (m_peakCache.count() > 0) {
-        auto sceneWidth = rectWidth * 1;
+        //        auto sceneWidth = rectWidth * 1;
+        //        auto start = rectWidth / 2;
+        //        auto end = rectWidth;
+        m_sceneWidth = rectWidth * m_scale;
+        auto start = m_renderStart;
+        auto end = m_renderEnd;
         auto drawPeak = [&](int x, double min, double max) {
             auto yMin = -min * halfRectHeight + halfRectHeight;
             auto yMax = -max * halfRectHeight + halfRectHeight;
             painter.drawLine(x, yMin, x, yMax);
         };
 
-        int divideCount = m_peakCache.count() / sceneWidth;
-        for (int i = 0; i < sceneWidth; i++) {
+        int divideCount = m_peakCache.count() / m_sceneWidth;
+        for (int i = 0; i < m_sceneWidth; i++) {
             double min = 0;
             double max = 0;
 
@@ -54,7 +80,7 @@ void WaveformWidget::paintEvent(QPaintEvent *event) {
                 if (frameMax > max)
                     max = frameMax;
             }
-            if (i == sceneWidth - 1) {
+            if (i == m_sceneWidth - 1) {
                 for (int j = i * (divideCount + 1); j < m_peakCache.count(); j++) {
                     auto rawFrame = m_peakCache.at(j);
                     auto frameMin = std::get<0>(rawFrame);
@@ -66,7 +92,8 @@ void WaveformWidget::paintEvent(QPaintEvent *event) {
                 }
             }
 
-            drawPeak(i, min, max);
+            double x = 1.0 * (i - start) / (end - start) * rectWidth;
+            drawPeak(x, min, max);
         }
     }
 
@@ -118,7 +145,98 @@ bool WaveformWidget::openFile(const QString &path) {
             m_peakCache.append(pair);
         }
     }
+
+    m_clipName = path;
+
     update();
 
     return true;
+}
+
+void WaveformWidget::resizeEvent(QResizeEvent *event) {
+//    if (m_rectLastWidth != -1) {
+//        if (m_renderEnd < rect().width() * m_scale) {
+//            m_renderEnd += (rect().width() - m_rectLastWidth) * m_scale;
+//        }
+//    }
+//
+//    if (m_rectLastWidth == -1)
+//        m_renderEnd = rect().width();
+//
+//    m_rectLastWidth = rect().width();
+    m_renderEnd = rect().width();
+
+    QWidget::resizeEvent(event);
+}
+
+void WaveformWidget::zoomIn(double factor) {
+    auto zoomedScale = m_scale * factor;
+    if (zoomedScale > 8)
+        return;
+    m_scale = zoomedScale;
+    m_renderEnd /= factor;
+    update();
+}
+
+void WaveformWidget::zoomOut(double factor) {
+    auto zoomedScale = m_scale / factor;
+    if (zoomedScale < 1)
+        return;
+    m_scale = zoomedScale;
+    m_renderEnd *= factor;
+    update();
+}
+
+bool WaveformWidget::eventFilter(QObject *object, QEvent *event) {
+
+    return QObject::eventFilter(object, event);
+}
+
+void WaveformWidget::wheelEvent(QWheelEvent *event) {
+    //    qDebug() << event->angleDelta();
+    auto mousePos = event->position();
+    auto angleY = event->angleDelta().y();
+    auto factor = 1 + 0.2 * qAbs(angleY) / 120;
+    if (angleY > 0) {
+        zoomIn(factor);
+    } else if (angleY < 0) {
+        zoomOut(factor);
+    }
+    qDebug() << m_scale;
+    QWidget::wheelEvent(event);
+}
+
+void WaveformWidget::mouseMoveEvent(QMouseEvent *event) {
+    auto dx = event->pos().x() - m_mouseLastPos.x();
+    //    qDebug() << dx;
+    auto scaledDx = dx / m_scale;
+    auto start = m_renderStart;
+    auto end = m_renderEnd;
+    //    qDebug() << start << end << m_sceneWidth;
+    if (start - dx > 0) {
+        //        m_renderStart -= scaledDx;
+        if (end - dx < m_sceneWidth) {
+            m_renderStart -= scaledDx;
+            m_renderEnd -= scaledDx;
+        } else {
+            m_renderEnd = m_sceneWidth;
+        }
+    } else {
+        m_renderStart = 0;
+    }
+    //    if (end - dx < m_sceneWidth)
+    //    {
+    //        m_renderEnd -= scaledDx;
+    //    } else {
+    //        m_renderEnd = m_sceneWidth;
+    //    }
+    m_mouseLastPos = event->pos();
+
+    update();
+    QWidget::mouseMoveEvent(event);
+}
+
+void WaveformWidget::mousePressEvent(QMouseEvent *event) {
+    m_mouseLastPos = event->pos();
+    QWidget::mousePressEvent(event);
 }
--- src/tests/WaveformTest/WaveformWidget.h
+++ src/tests/WaveformTest/WaveformWidget.h
@@ -7,6 +7,7 @@
 
 #include <QFrame>
 #include <QThread>
+#include <QWheelEvent>
 
 class WaveformWidget : public QFrame {
     Q_OBJECT
@@ -16,12 +17,26 @@ public:
     ~WaveformWidget() override;
 
     bool openFile(const QString &path);
+    void zoomIn(double factor = 1.2);
+    void zoomOut(double factor = 1.2);
 
 protected:
     QVector<std::tuple<double, double>> m_peakCache;
+    int m_sceneWidth = 0;
+    double m_renderStart = 0;
+    double m_renderEnd = 0;
+    double m_scale = 1.0;
+//    int m_zoomInLevel = 0;
+    QString m_clipName;
+    QPoint m_mouseLastPos;
+    int m_rectLastWidth = -1;
 
-    //    bool eventFilter(QObject *object, QEvent *event) override;
+    bool eventFilter(QObject *object, QEvent *event) override;
+    void wheelEvent(QWheelEvent *event) override;
     void paintEvent(QPaintEvent *event) override;
+    void resizeEvent(QResizeEvent *event) override;
+    void mousePressEvent(QMouseEvent *event) override;
+    void mouseMoveEvent(QMouseEvent *event) override;
 };
 
 #endif // DATASET_TOOLS_WAVEFORMWIDGET_H
