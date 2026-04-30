4a1d7a3eabedfc6eaeefd3e6141b718ee7c2c1c3
wolfgitpr <133209402+wolfgitpr@users.noreply.github.com>
2026-04-30 12:43:37 +0800

    refactor(T-19): create GameInferPage implementing IPageActions
    - Extract menu/statusbar/drag-drop from MainWindow into GameInferPage
    - Wraps MainWidget as central content
    - Add labeler-interfaces link to GameInfer CMakeLists
    - Prepares GameInfer for AppShell migration

---
+++ src/apps/GameInfer/gui/GameInferPage.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <IPageActions.h>
+
+#include <QWidget>
+
+class QProgressBar;
+class MainWidget;
+
+namespace dstools {
+class AppSettings;
+}
+
+class GameInferPage : public QWidget, public dstools::labeler::IPageActions {
+    Q_OBJECT
+    Q_INTERFACES(dstools::labeler::IPageActions)
+
+public:
+    explicit GameInferPage(dstools::AppSettings *settings, QWidget *parent = nullptr);
+
+    // IPageActions
+    QMenuBar *createMenuBar(QWidget *parent) override;
+    QWidget *createStatusBarContent(QWidget *parent) override;
+    QString windowTitle() const override;
+    bool supportsDragDrop() const override;
+    void handleDragEnter(QDragEnterEvent *event) override;
+    void handleDrop(QDropEvent *event) override;
+
+private:
+    MainWidget *m_mainWidget;
+    dstools::AppSettings *m_settings;
+};
