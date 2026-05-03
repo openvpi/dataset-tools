08593d8fc1f0787b7ee9a3647e418c569b849aad
wolfgitpr <133209402+wolfgitpr@users.noreply.github.com>
2026-05-03 10:18:40 +0800

    feat(N.3): add zh_CN translations for domain layer (dstools-widgets)
    Populate dstools-widgets_zh_CN.ts with translations for widget strings.

--- src/widgets/translations/dstools-widgets_zh_CN.ts
+++ src/widgets/translations/dstools-widgets_zh_CN.ts
@@ -1,5 +1,15 @@
 ﻿<?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.1" language="zh_CN">
+<context>
+    <name>dstools::widgets</name>
+    <message>
+        <source>Load</source>
+        <translation>加载</translation>
+    </message>
+    <message>
+        <source>Loading...</source>
+        <translation>加载中...</translation>
+    </message>
+</context>
 </TS>
-
