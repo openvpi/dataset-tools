26b5da77ca1b7ef528a8ac6fe9849d7df56249bc
wolfgitpr <133209402+wolfgitpr@users.noreply.github.com>
2026-05-13 19:24:18 +0800

    [widgets] Split RunProgressRow out of SimpleDialogs.cpp into own source file

--- src/framework/widgets/src/SimpleDialogs.cpp
+++ src/framework/widgets/src/SimpleDialogs.cpp
@@ -1,11 +1,9 @@
 #include <dsfw/widgets/SettingsDialog.h>
 #include <dsfw/widgets/ProgressDialog.h>
-#include <dsfw/widgets/RunProgressRow.h>
 #include <dsfw/widgets/PropertyEditor.h>
 #include <dsfw/AppSettings.h>
 
 #include <QDialogButtonBox>
-#include <QHBoxLayout>
 #include <QLabel>
 #include <QProgressBar>
 #include <QPushButton>
@@ -100,52 +98,6 @@ QString ProgressDialog::labelText() const {
 
 void ProgressDialog::setCancelButtonEnabled(bool enabled) {
     m_cancelBtn->setEnabled(enabled);
-}
-
-// ─── RunProgressRow ──────────────────────────────────────────────────────────
-
-RunProgressRow::RunProgressRow(const QString &buttonText, QWidget *parent)
-    : QWidget(parent), m_runText(buttonText) {
-    auto *layout = new QHBoxLayout(this);
-    layout->setContentsMargins(0, 0, 0, 0);
-
-    m_button = new QPushButton(m_runText, this);
-    m_progressBar = new QProgressBar(this);
-    m_progressBar->setRange(0, 100);
-    m_progressBar->setValue(0);
-    m_progressBar->setVisible(false);
-
-    layout->addWidget(m_button);
-    layout->addWidget(m_progressBar, 1);
-
-    connect(m_button, &QPushButton::clicked, this, [this]() {
-        if (m_running)
-            emit cancelClicked();
-        else
-            emit runClicked();
-    });
-}
-
-void RunProgressRow::setProgress(int percent) {
-    m_progressBar->setValue(qBound(0, percent, 100));
-}
-
-void RunProgressRow::setRunning(bool running) {
-    m_running = running;
-    m_button->setText(running ? tr("Cancel") : m_runText);
-    m_progressBar->setVisible(running);
-    if (!running)
-        m_progressBar->setValue(0);
-}
-
-void RunProgressRow::setButtonEnabled(bool enabled) {
-    m_button->setEnabled(enabled);
-}
-
-void RunProgressRow::reset() {
-    setRunning(false);
-    setButtonEnabled(true);
-    m_progressBar->setValue(0);
 }
 
 } // namespace dsfw::widgets
